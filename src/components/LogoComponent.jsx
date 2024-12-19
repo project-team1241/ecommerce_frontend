8b28c93fff49680aa075d58a7cdf6207e71817f6
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-12-19 12:24:51 +0530

    Mainlayout grid added, login component added

--- src/components/LogoComponent.jsx
+++ src/components/LogoComponent.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function LogoComponent() {
-    return <div className="w-[436px] h-[500px] bg-[#31937C]"></div>;
+    return (
+        <div className="w-[436px] h-[500px] bg-gradient-to-t from-[#31937C] via-[#31937C] to-[#D9EFEA] rounded-[4px]"></div>
+    );
 }
 
 export default LogoComponent;
