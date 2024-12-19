79a0656d76204e93e81105dfc32474c3b17086ec
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-12-19 11:52:04 +0530

    Logo component has been added

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button({ name }) {
+    return (
+        <div>
+            <button type="submit">{name}</button>
+        </div>
+    );
+}
+
+export default Button;
---
+++ src/components/LogoComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LogoComponent() {
+    return <div className="w-[436px] h-[500px] bg-[#31937C]"></div>;
+}
+
+export default LogoComponent;
