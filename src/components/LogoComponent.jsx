bf1ea976f4ef94766168cb759575c00e27cc15e6
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-12-19 14:57:00 +0530

    register page added

--- src/components/LogoComponent.jsx
+++ src/components/LogoComponent.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function LogoComponent() {
     return (
-        <div className="w-[436px] h-[500px] bg-gradient-to-t from-[#31937C] via-[#31937C] to-[#D9EFEA] rounded-[4px]"></div>
+        <div className="w-[436px] h-[500px] bg-gradient-to-t from-[#31937C] via-[#31937C] to-[#D9EFEA] rounded-[4px]">
+            <p className="flex flex-col justify-center items-center text-[90px]">Logo</p>
+        </div>
     );
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,11 +4,23 @@ import LogoComponent from "../components/LogoComponent";
 
 function Register() {
     return (
-        <div className="grid row-1 cols-2">
+        <div className="grid grid-cols-2 p-[40px]">
             <div>
                 <LogoComponent></LogoComponent>
             </div>
             <form action="POST">
+                <div className="w-[10px]">
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        Email address
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="john.doe@company.com"
+                        required
+                    />
+                </div>
                 <Button name={Register} />
             </form>
         </div>
