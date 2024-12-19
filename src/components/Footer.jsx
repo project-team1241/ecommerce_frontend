3318d66049d55ba0578d280f03bbec8ffe4e08e3
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-12-19 16:07:50 +0530

    register component added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="bg-gray-700 text-white">Footer
-  </div>;
+    return (
+        <div className="bg-gray-700 h-[255px] text-white">
+            <section>Logo</section>
+            <section>Logo</section>
+            <section>Logo</section>
+            <section>Logo</section>
+        </div>
+    );
 };
 
 export default Footer;
